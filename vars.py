6dff7810d41cff2d2752b5c83db827d008e505ba
Theboys23456 <krishna93107437@gmail.com>
2025-05-30 01:12:57 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,12 @@
 #🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
+from os import environ
 
-API_ID    = os.environ.get("API_ID", "26328207")
-API_HASH  = os.environ.get("API_HASH", "d6dcc13df7527b36df3a28e04a72b35b")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "28094744"))
+API_HASH = environ.get("API_HASH", "a75af4285edc7747c57bb19147ca0b9b")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+OWNER = int(environ.get("OWNER", "6158571763"))
+CREDIT = "Departmentof telegram"
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
