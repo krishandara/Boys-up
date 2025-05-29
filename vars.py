1aedeaca569bff47b276b51a7cdc94817abbf1d4
Theboys23456 <krishna93107437@gmail.com>
2025-05-30 01:23:19 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,7 +6,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", "28094744"))
 API_HASH = environ.get("API_HASH", "a75af4285edc7747c57bb19147ca0b9b")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "6158571763"))
+OWNER = int(environ.get("OWNER", "6126688051"))
 CREDIT = "Departmentof telegram"
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
